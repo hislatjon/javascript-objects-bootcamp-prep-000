760611c7b8b95bf9b3daff19b4c9cd884ae15826
Hislatjon Sadizoda <hislatjon@gmail.com>
2018-10-10 18:58:19 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
 var playlist = {artistName: "songTitle"}
 function updatePlaylist(playlist, artistName, songTitle){
-  return object[key] = artistName: songTitle
+  return playlist[key] = artistName: songTitle
 }
